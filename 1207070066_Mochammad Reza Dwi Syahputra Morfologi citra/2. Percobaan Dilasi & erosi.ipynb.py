4b8ba2ef5c4a0d76e2f1776ce5f8059ab9c8f67f
Mochrezads <133202210+Mochrezads@users.noreply.github.com>
2023-06-26 22:53:20 +0700

    Add files via upload

---
+++ 1207070066_Mochammad Reza Dwi Syahputra Morfologi citra/2. Percobaan Dilasi & erosi.ipynb.py
@@ -0,0 +1,23 @@
+import cv2 
+import numpy as np 
+import matplotlib.pyplot as plt
+#%matplotlib inline
+fig, axes = plt.subplots(3, 2, figsize=(20, 20))
+ax = axes.ravel()
+
+ax[0].imshow(img, cmap = 'gray')
+ax[0].set_title("Citra Input")
+ax[1].hist(img.ravel(), bins = 256)
+ax[1].set_title("Histogram Citra Input")
+
+ax[2].imshow(img_erosion, cmap = 'gray')
+ax[2].set_title("Citra Output Erosi")
+ax[3].hist(img_erosion.ravel(), bins = 256)
+ax[3].set_title("Histogram Citra Output Erosi")
+
+ax[4].imshow(img_dilation, cmap = 'gray')
+ax[4].set_title("Citra Output Dilasi")
+ax[5].hist(img_dilation.ravel(), bins = 256)
+ax[5].set_title("Histogram Citra Output Erosi")
+plt.show()
+
